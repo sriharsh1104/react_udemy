e6ed9c2423fba23b3072e9d1b0773c471ef20e63
sriharsh1103 <prashant.sriharsh@antiersolutions.com>
2025-11-06 13:51:51 +0530

    fix:removed andriod

--- android/app/src/main/java/com/reactudemy/toolbox/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.reactudemy.toolbox;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.reactudemy.toolbox',
-  appName: 'All In One ToolBox',
-  webDir: 'dist'
-};
-
-export default config;
